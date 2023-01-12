b468066ea461d42b6ecbc4be3c59f7862285a36e
shangwenhe <shangwenhe@rongcloud.cn>
2023-01-12 13:36:35 +0800

    FEAT: 新功能实现相关

--- src/pages/wechat/call/index.tsx
+++ src/pages/wechat/call/index.tsx
@@ -7,48 +7,54 @@ const DiscoverVideo = () => {
   const videoTag = useRef<HTMLVideoElement>(null)
   useEffect(()=>{
     const constraints = {
-      audio: true,
+      audio: false,
       video: { width: window.innerWidth, height: window.outerHeight }
     };
 
     navigator.mediaDevices.getUserMedia(constraints)
     .then(function(mediaStream) {
       const video = videoTag.current;
-      // if (video) {
-      //   video.srcObject = mediaStream;
-      //   video.onloadedmetadata = function(e) {
-      //     video.play();
-      //   };
-      // }
+      if (video) {
+        video.srcObject = mediaStream;
+        video.onloadedmetadata = function(e) {
+          video.play();
+        };
+      }
     })
     .catch(function(err) { console.log(err.name + ": " + err.message); })
-
+    return () => {
+    }
   }, [])
   const [ soundMute, setSoundMute ] = useState<boolean>(false);
   const [ audioMute, setAudioMute ] = useState<boolean>(false);
   const [ phoneStatus, setPhoneStatus ] = useState<boolean>(false);
+
+  const hangup = () => {
+    setPhoneStatus(false);
+    history.back()
+  }
   return <>
     <video ref={videoTag} ></video>
-    <Space direction='vertical' block style={{ '--gap': '1em', backgroundColor: "var(--adm-color-box)" }}>
-      <Grid className="user-grid" columns={3} gap={8} style={{'padding': '0px 8px'}}>
+    <Space className="call-actions" direction='vertical' block style={{ '--gap': '1em', backgroundColor: "transparent" }}>
+      <Grid className="user-grid" columns={3} gap={8}>
           <Grid.Item>
             <span className="user-grid-item">
-              <CameraOutline />
+              <CameraOutline color="#ffffff" />
             </span>
           </Grid.Item>
           <Grid.Item>
             <span className={`user-grid-item ${soundMute && 'red'}`} onClick={()=> setSoundMute(!soundMute) }>
-              { soundMute? <SoundMuteOutline color="var(--adm-color-danger)" /> :  <SoundOutline /> }
+              { soundMute? <SoundMuteOutline color="var(--adm-color-danger)" /> :  <SoundOutline color="#ffffff" /> }
             </span>
           </Grid.Item>
           <Grid.Item>
             <span className={`user-grid-item ${audioMute && 'red'}`} onClick={()=> setAudioMute(!audioMute) }>
-              { audioMute ? <AudioMutedOutline color="var(--adm-color-danger)" /> : <AudioOutline /> }
+              { audioMute ? <AudioMutedOutline color="var(--adm-color-danger)" /> : <AudioOutline color="#ffffff" /> }
             </span>
           </Grid.Item>
           <Grid.Item span={3}>
-            <span className={`user-grid-item ${phoneStatus && 'red'}`} onTouchStart={()=> setPhoneStatus(true)} onTouchEnd={()=> setPhoneStatus(false)}>
-              <PhoneFill color={ phoneStatus ? 'red' : '#000' } />
+            <span className={`user-grid-item ${phoneStatus && 'red'}`} onTouchStart={()=> setPhoneStatus(true)} onTouchEnd={hangup}>
+              <PhoneFill color={ phoneStatus ? 'red' : '#ffffff' } />
             </span>
           </Grid.Item>
         </Grid>
